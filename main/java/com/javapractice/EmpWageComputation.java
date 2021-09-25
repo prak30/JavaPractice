a0f9b7a62e31066a64d5de04dc647597dbd0b502
pranav <prnvkatkar88@gmail.com>
2021-09-25 11:30:28 +0530

    Added:Welcome

---
+++ main/java/com/javapractice/EmpWageComputation.java
@@ -0,0 +1,10 @@
+package com.javapractice;
+
+public class EmpWageComputation
+{	
+	public static void main(String[] args)
+	{
+		System.out.println("Welcome To Employee Wage Computation");
+	}
+}
+	
